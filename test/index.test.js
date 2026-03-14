08149f7967ec5dc9cf0554802f13be82b5471acc
April Lindie <april@MacBookPro.lan>
2026-03-14 00:25:09 -0400

    completed jest eslint lab

--- test/index.test.js
+++ test/index.test.js
@@ -1,2 +1,7 @@
 const { capitalizeWords, filterActiveUsers, logAction } = require('../index')
 
+describe("capitalizeWords", () => {
+  test("capitalizes each word in a string", () => {
+    expect(capitalizeWords("hello world")).toBe("Hello World")
+  })
+})
